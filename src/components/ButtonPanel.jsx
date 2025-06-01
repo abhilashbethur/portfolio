328a4be0f3b1a6ef23e978d0e907005e513d1a60
abhilashbethur <abhilashbethur@gmail.com>
2025-06-01 12:32:27 +0530

    added readme and button logic

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -7,31 +7,32 @@ const ButtonPanel = ({
   handleFloorClick,
   currentFloor,
 }) => {
+  const isDisabledOpenDoorBtn = isMoving || doorsOpen || currentFloor === 0;
+  const isDisabledCloseDoorBtn = isMoving || !doorsOpen;
   return (
     <>
       <div className="w-[60px] bg-[#2e2d3e] border-[3px] border-[#5a556e] shadow-[inset_0_0_20px_rgba(255,255,255,0.1)] rounded-md flex flex-col-reverse gap-6 p-10 mt-20 justify-center items-center">
         {/* Open Door Button */}
         <button
           onClick={() => setDoorsOpen(true)}
-          disabled={isMoving || doorsOpen}
+          disabled={isDisabledOpenDoorBtn}
           className={`w-10 h-10 border rounded-md text-base shadow-[0_2px_4px_rgba(0,0,0,0.5)] transition ${
-            doorsOpen
-              ? "bg-green-600 text-white border-green-800 cursor-not-allowed"
-              : "bg-gradient-to-b from-[#44475a] to-[#2e2e42] border-[#5a556e] text-[#e0d8c3] hover:bg-gradient-to-b hover:from-[#6b6f91] hover:to-[#474a68] cursor-pointer"
-          } ${isMoving && "opacity-50 cursor-not-allowed"}`}
+            isDisabledOpenDoorBtn
+              ? "bg-green-600 text-white border-green-800 cursor-not-allowed opacity-50"
+              : "bg-gradient-to-b from-[#44475a] to-[#2e2e42] border-[#5a556e] text-[#e0d8c3] hover:from-[#6b6f91] hover:to-[#474a68] cursor-pointer"
+          }`}
         >
           {"<>"}
         </button>
 
-        {/* Close Door Button */}
         <button
           onClick={() => setDoorsOpen(false)}
-          disabled={isMoving || !doorsOpen}
+          disabled={isDisabledCloseDoorBtn}
           className={`w-10 h-10 border rounded-md text-base shadow-[0_2px_4px_rgba(0,0,0,0.5)] transition ${
-            !doorsOpen
-              ? "bg-green-600 text-white border-green-800 cursor-not-allowed"
-              : "bg-gradient-to-b from-[#44475a] to-[#2e2e42] border-[#5a556e] text-[#e0d8c3] hover:bg-gradient-to-b hover:from-[#6b6f91] hover:to-[#474a68] cursor-pointer"
-          } ${isMoving && "opacity-50 cursor-not-allowed"}`}
+            isDisabledCloseDoorBtn
+              ? "bg-green-600 text-white border-green-800 cursor-not-allowed opacity-50"
+              : "bg-gradient-to-b from-[#44475a] to-[#2e2e42] border-[#5a556e] text-[#e0d8c3] hover:from-[#6b6f91] hover:to-[#474a68] cursor-pointer"
+          }`}
         >
           {"><"}
         </button>
